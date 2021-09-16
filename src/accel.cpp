de88944360b7f76a624725bb5738e9cb44072d3c
Hajin Choi <hchoi405@gmail.com>
2021-09-16 13:24:22 +0900

    Indentation

--- src/accel.cpp
+++ src/accel.cpp
@@ -233,53 +233,50 @@ bool Accel::rayIntersect(const Ray3f &ray_, Intersection &its,
            characterize the intersection (normals, texture coordinates, etc..)
         */
 
-        /* Find the barycentric coordinates */
-        Vector3f bary;
-        bary << 1-its.uv.sum(), its.uv;
+    /* Find the barycentric coordinates */
+    Vector3f bary;
+    bary << 1 - its.uv.sum(), its.uv;
 
-        /* References to all relevant mesh buffers */
-        const Mesh *mesh   = its.mesh;
-        const MatrixXf &V  = mesh->getVertexPositions();
-        const MatrixXf &N  = mesh->getVertexNormals();
-        const MatrixXf &UV = mesh->getVertexTexCoords();
-        const MatrixXu &F  = mesh->getIndices();
+    /* References to all relevant mesh buffers */
+    const Mesh *mesh = its.mesh;
+    const MatrixXf &V = mesh->getVertexPositions();
+    const MatrixXf &N = mesh->getVertexNormals();
+    const MatrixXf &UV = mesh->getVertexTexCoords();
+    const MatrixXu &F = mesh->getIndices();
 
-        /* Vertex indices of the triangle */
-        uint32_t idx0 = F(0, f), idx1 = F(1, f), idx2 = F(2, f);
+    /* Vertex indices of the triangle */
+    uint32_t idx0 = F(0, f), idx1 = F(1, f), idx2 = F(2, f);
 
-        Point3f p0 = V.col(idx0), p1 = V.col(idx1), p2 = V.col(idx2);
+    Point3f p0 = V.col(idx0), p1 = V.col(idx1), p2 = V.col(idx2);
 
-        /* Compute the intersection positon accurately
-           using barycentric coordinates */
-        its.p = bary.x() * p0 + bary.y() * p1 + bary.z() * p2;
+    /* Compute the intersection positon accurately
+       using barycentric coordinates */
+    its.p = bary.x() * p0 + bary.y() * p1 + bary.z() * p2;
 
-        /* Compute proper texture coordinates if provided by the mesh */
-        if (UV.size() > 0)
-            its.uv = bary.x() * UV.col(idx0) +
-                bary.y() * UV.col(idx1) +
-                bary.z() * UV.col(idx2);
+    /* Compute proper texture coordinates if provided by the mesh */
+    if (UV.size() > 0)
+      its.uv = bary.x() * UV.col(idx0) + bary.y() * UV.col(idx1) +
+               bary.z() * UV.col(idx2);
 
-        /* Compute the geometry frame */
-        its.geoFrame = Frame((p1-p0).cross(p2-p0).normalized());
+    /* Compute the geometry frame */
+    its.geoFrame = Frame((p1 - p0).cross(p2 - p0).normalized());
 
-        if (N.size() > 0) {
-            /* Compute the shading frame. Note that for simplicity,
-               the current implementation doesn't attempt to provide
-               tangents that are continuous across the surface. That
-               means that this code will need to be modified to be able
-               use anisotropic BRDFs, which need tangent continuity */
+    if (N.size() > 0) {
+      /* Compute the shading frame. Note that for simplicity,
+         the current implementation doesn't attempt to provide
+         tangents that are continuous across the surface. That
+         means that this code will need to be modified to be able
+         use anisotropic BRDFs, which need tangent continuity */
 
-            its.shFrame = Frame(
-                (bary.x() * N.col(idx0) +
-                 bary.y() * N.col(idx1) +
-                 bary.z() * N.col(idx2)).normalized());
-        } else {
-            its.shFrame = its.geoFrame;
-        }
+      its.shFrame = Frame((bary.x() * N.col(idx0) + bary.y() * N.col(idx1) +
+                           bary.z() * N.col(idx2))
+                              .normalized());
+    } else {
+      its.shFrame = its.geoFrame;
     }
+  }
 
-    return foundIntersection;
+  return foundIntersection;
 }
 
 NORI_NAMESPACE_END
-
